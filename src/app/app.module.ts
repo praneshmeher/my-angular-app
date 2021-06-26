57617190805d49a3bdd857c27c5caa2cbb1dd0fb
Pranesh <praneshmeher@gmail.com>
2021-06-26 17:47:45 +0530

    commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,13 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms'
 
 import { AppComponent } from './app.component';
 import { AttributeComponent } from './attribute/attribute.component';
 import { StringInterpolationComponent } from './string-interpolation/string-interpolation.component';
 import { PropertyBindingComponent } from './property-binding/property-binding.component';
 import { EventBindingComponent } from './event-binding/event-binding.component';
+import { TwoWayComponent } from './two-way/two-way.component';
 
 @NgModule({
   declarations: [
@@ -13,10 +15,12 @@ import { EventBindingComponent } from './event-binding/event-binding.component';
     AttributeComponent,
     StringInterpolationComponent,
     PropertyBindingComponent,
-    EventBindingComponent
+    EventBindingComponent,
+    TwoWayComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
