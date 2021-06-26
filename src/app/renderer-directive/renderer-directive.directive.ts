80a1e534e20235c0748d09f9efe47f925c77f74e
Pranesh <praneshmeher@gmail.com>
2021-06-27 00:25:22 +0530

    commit

--- src/app/renderer-directive/renderer-directive.directive.ts
+++ src/app/renderer-directive/renderer-directive.directive.ts
@@ -1,18 +1,20 @@
-import { Directive, Renderer2, OnInit, ElementRef, HostListener } from '@angular/core';
+import { Directive, Renderer2, OnInit, ElementRef, HostListener, HostBinding } from '@angular/core';
 
 @Directive({
   selector: '[appRendererDirective]'
 })
 export class RendererDirectiveDirective implements OnInit {
 
+  @HostBinding('style.background') color:any;
   constructor(private elementRef:ElementRef, private renderer:Renderer2) { }
 
   ngOnInit(){
     this.renderer.setStyle(this.elementRef.nativeElement, 'color', 'red')
   }
 
-  @HostListener('click') click(event:any){
+  @HostListener('click') onClick(event:any){
     console.log('clicked')
+    this.color='green'
   }
   
 }
