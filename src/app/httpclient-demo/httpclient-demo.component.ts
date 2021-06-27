55f058cce5269ecc7acf548a5b99fb8e62d7d4ef
Pranesh <praneshmeher@gmail.com>
2021-06-27 20:09:38 +0530

    commit

--- src/app/httpclient-demo/httpclient-demo.component.ts
+++ src/app/httpclient-demo/httpclient-demo.component.ts
@@ -11,12 +11,15 @@ export class HttpclientDemoComponent implements OnInit {
   constructor(private http:HttpClient) { }
 
   ngOnInit(): void {
-    this.http.post("url",'data')
+    this.http.post("https://reqres.in/api/users",{
+      "name": "morpheus",
+      "job": "leader"
+    })
     .subscribe((data)=>{
       console.log(data)
     })
 
-    this.http.get("urel")
+    this.http.get("https://reqres.in/api/users?page=2")
     .subscribe((data)=>{
       console.log(data)
     })
